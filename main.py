bb9582ed08b9d02c33b2df216ac577d31d3b56c2
Claudiu Sava <claudiu.sava@protonmail.com>
2022-02-20 13:32:28 +0100

    v1.0

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os.path
 import random
 from tkinter import *
+import platform
+import pathlib
+import os
+from PIL import Image, ImageTk
+
+
+os_name = platform.system()
+
 
 
 window = Tk()
@@ -8,20 +17,29 @@ window.title("Dice Rolling Simulator")
 gridFrame = Frame(window)
 gridFrame.pack(side=TOP)
 
+programPath = pathlib.Path(__file__).parent.absolute()
+
 
 def play():
     diceNumber = random.randint(1, 6)
     yourNumberIsLabel = Label(gridFrame, text="Your number is " + str(diceNumber) + "!", font=(None, 15))
     yourNumberIsLabel.grid(column=0, row=0)
 
-    diceImage = PhotoImage(file = "img/dice%s.png" % diceNumber)
-    imgButton = Button(gridFrame, image=diceImage)
-    imgButton.grid(column=0, row=1)
+    showDiceIcon(diceNumber)
 
     newNumberButton = Button(gridFrame, text="Roll Again", command = lambda: play())
     newNumberButton.grid(column=0, row=2, pady=15)
 
     window.mainloop()
+
+
+def showDiceIcon(diceNumber):
+    load = Image.open(str(programPath) + os.path.sep + "icons" + os.path.sep + "dice" + str(diceNumber) + ".png" )
+    render = ImageTk.PhotoImage(load)
+
+    img = Label(gridFrame, image=render)
+    img.image = render
+    img.grid(column=0, row=1)
 
 
 play()
